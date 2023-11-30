a1ff885b7e04200a1a975ffae7eb3c1cca522535
afonsogoiaba <afonso33barbosa@gmail.com>
2023-11-30 14:04:03 -0400

    adicionado onInit e removido dados estaticos

--- src/app/components/datatable/datatable.component.ts
+++ src/app/components/datatable/datatable.component.ts
@@ -1,38 +1,21 @@
-import { Component  } from '@angular/core';
-import { HttpService } from 'src/app/services/http.service';
+import { Component, OnInit  } from '@angular/core';
 
-interface Relatorio {
-  fornec_id: number,
-  nome: string,
-  quantidade: number,
-  total: number
-}
+import { HttpService } from 'src/app/services/http.service';
+import { DataSourceService } from 'src/app/services/data-source.service';
 
 @Component({
   selector: 'app-datatable',
   templateUrl: './datatable.component.html',
   styleUrls: ['./datatable.component.scss']
 })
-export class DatatableComponent {
+export class DatatableComponent implements OnInit {
 
-  dados: Relatorio[] = [
-    {fornec_id: 1, nome: 'GRAM INDUSTRIA E COMERCIO LTDA', quantidade: 50745, total: 1456515.95},
-    {fornec_id: 8, nome: '8 GRAM INDUSTRIA E COMERCIO LTDA', quantidade: 26048, total: 440487.59},
-    {fornec_id: 5, nome: 'ANTONELLA COSMETICOS EIRELI', quantidade: 9327, total: 154559.27},
-    {fornec_id: 7, nome: 'GRAM INDUSTRIA E COMERCIO LTDA', quantidade: 5368, total: 35493.2}
-  ];
-
-  constructor(private httpservice: HttpService){
-    this.obterDados();
-  }
-
-  obterDados(){
-    this.httpservice.getRelatorio().subscribe(data =>{
-      console.log(data);
-      this.dados = data;
-    })
-  }
+  constructor(private httpservice: HttpService){}
 
   displayedColumns: string[] = ['fornec_id', 'nome', 'quantidade', 'total'];
-  dataSource = this.dados;
+  dataSource = new DataSourceService(this.httpservice);
+
+  ngOnInit(): void {
+    this.dataSource.loadRelatorio()
+  }
 }
